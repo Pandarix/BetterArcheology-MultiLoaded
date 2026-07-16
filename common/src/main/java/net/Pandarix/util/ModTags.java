3bcb019e08a58e48ae24dbc99d5513f320b586c3
ruven <mail@ruven.dev>
2026-07-16 14:15:45 +0200

    fix: add bomb entity lang key, register music disc convention tag (#8, #30)

--- common/src/main/java/net/Pandarix/util/ModTags.java
+++ common/src/main/java/net/Pandarix/util/ModTags.java
@@ -21,6 +21,8 @@ public class ModTags {
 
         public static final TagKey<Item> ELYTRAS = TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("c", "elytras"));
 
+        public static final TagKey<Item> MUSIC_DISCS = TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("c", "music_discs"));
+
         public static final TagKey<Item> ROTTEN_LOGS = tag("rotten_logs");
 
         public static final TagKey<Item> TUNNELING_ITEMS = tag("tunneling_items");
